cee452db4a20eb47defcfb235e0cf4f84767ccb4
hungdeptraino1 <97597945+hungdeptraino1@users.noreply.github.com>
2024-04-17 01:30:58 +0700

    Add files via upload

---
+++ ASM2/ASM2/WaterBill.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace ASM2
+{
+    partial class WaterBill
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            btnLogout = new Button();
+            groupBox1 = new GroupBox();
+            label8 = new Label();
+            label7 = new Label();
+            txtWatercon = new TextBox();
+            txtMoney = new TextBox();
+            btnCal = new Button();
+            btnClear = new Button();
+            btnEdit = new Button();
+            btnAdd = new Button();
+            txtCurrentMonth = new TextBox();
+            label6 = new Label();
+            txtLastMonthWater = new TextBox();
+            label5 = new Label();
+            txtMember = new TextBox();
+            label4 = new Label();
+            cobCustomerType = new ComboBox();
+            label3 = new Label();
+            txtFullName = new TextBox();
+            label2 = new Label();
+            groupBox2 = new GroupBox();
+            lstVBill = new ListView();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(250, 30);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(268, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome to Water Bill";
+            // 
+            // btnLogout
+            // 
+            btnLogout.Location = new Point(741, 0);
+            btnLogout.Margin = new Padding(2, 1, 2, 1);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(109, 31);
+            btnLogout.TabIndex = 1;
+            btnLogout.Text = "Logout";
+            btnLogout.UseVisualStyleBackColor = true;
+            btnLogout.Click += btnLogout_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(txtWatercon);
+            groupBox1.Controls.Add(txtMoney);
+            groupBox1.Controls.Add(btnCal);
+            groupBox1.Controls.Add(btnClear);
+            groupBox1.Controls.Add(btnEdit);
+            groupBox1.Controls.Add(btnAdd);
+            groupBox1.Controls.Add(txtCurrentMonth);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(txtLastMonthWater);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(txtMember);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(cobCustomerType);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(txtFullName);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(29, 81);
+            groupBox1.Margin = new Padding(2, 1, 2, 1);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(2, 1, 2, 1);
+            groupBox1.Size = new Size(867, 159);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Information of customer";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(571, 76);
+            label8.Name = "label8";
+            label8.Size = new Size(114, 15);
+            label8.TabIndex = 18;
+            label8.Text = "Water Consumption";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(571, 37);
+            label7.Name = "label7";
+            label7.Size = new Size(44, 15);
+            label7.TabIndex = 17;
+            label7.Text = "Money";
+            // 
+            // txtWatercon
+            // 
+            txtWatercon.Location = new Point(700, 73);
+            txtWatercon.Name = "txtWatercon";
+            txtWatercon.Size = new Size(100, 23);
+            txtWatercon.TabIndex = 16;
+            // 
+            // txtMoney
+            // 
+            txtMoney.Location = new Point(700, 29);
+            txtMoney.Name = "txtMoney";
+            txtMoney.Size = new Size(100, 23);
+            txtMoney.TabIndex = 15;
+            txtMoney.TextChanged += txtMoney_TextChanged;
+            // 
+            // btnCal
+            // 
+            btnCal.Location = new Point(265, 107);
+            btnCal.Name = "btnCal";
+            btnCal.Size = new Size(81, 23);
+            btnCal.TabIndex = 14;
+            btnCal.Text = "Calculate";
+            btnCal.UseVisualStyleBackColor = true;
+            btnCal.Click += btnCal_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(521, 108);
+            btnClear.Margin = new Padding(2, 1, 2, 1);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(81, 22);
+            btnClear.TabIndex = 13;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(436, 108);
+            btnEdit.Margin = new Padding(2, 1, 2, 1);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(81, 22);
+            btnEdit.TabIndex = 12;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
+            btnEdit.Click += btnEdit_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(351, 108);
+            btnAdd.Margin = new Padding(2, 1, 2, 1);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(81, 22);
+            btnAdd.TabIndex = 10;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // txtCurrentMonth
+            // 
+            txtCurrentMonth.Location = new Point(444, 63);
+            txtCurrentMonth.Margin = new Padding(2, 1, 2, 1);
+            txtCurrentMonth.Name = "txtCurrentMonth";
+            txtCurrentMonth.Size = new Size(91, 23);
+            txtCurrentMonth.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(265, 71);
+            label6.Margin = new Padding(2, 0, 2, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(175, 15);
+            label6.TabIndex = 8;
+            label6.Text = "Current Month Water's Number";
+            label6.Click += label6_Click;
+            // 
+            // txtLastMonthWater
+            // 
+            txtLastMonthWater.Location = new Point(444, 28);
+            txtLastMonthWater.Margin = new Padding(2, 1, 2, 1);
+            txtLastMonthWater.Name = "txtLastMonthWater";
+            txtLastMonthWater.Size = new Size(91, 23);
+            txtLastMonthWater.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(265, 32);
+            label5.Margin = new Padding(2, 0, 2, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(156, 15);
+            label5.TabIndex = 6;
+            label5.Text = "Last Month Water's Number";
+            // 
+            // txtMember
+            // 
+            txtMember.Enabled = false;
+            txtMember.Location = new Point(124, 108);
+            txtMember.Margin = new Padding(2, 1, 2, 1);
+            txtMember.Name = "txtMember";
+            txtMember.Size = new Size(94, 23);
+            txtMember.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(19, 110);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Member";
+            // 
+            // cobCustomerType
+            // 
+            cobCustomerType.FormattingEnabled = true;
+            cobCustomerType.Items.AddRange(new object[] { "Household customer", "Public services", "Production units", "Business services" });
+            cobCustomerType.Location = new Point(124, 68);
+            cobCustomerType.Margin = new Padding(2, 1, 2, 1);
+            cobCustomerType.Name = "cobCustomerType";
+            cobCustomerType.Size = new Size(94, 23);
+            cobCustomerType.TabIndex = 3;
+            cobCustomerType.SelectedIndexChanged += cobCustomerType_SelectedIndexChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(13, 71);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Customer Type";
+            // 
+            // txtFullName
+            // 
+            txtFullName.Location = new Point(124, 28);
+            txtFullName.Margin = new Padding(2, 1, 2, 1);
+            txtFullName.Name = "txtFullName";
+            txtFullName.Size = new Size(94, 23);
+            txtFullName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(26, 31);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Full name";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(lstVBill);
+            groupBox2.Location = new Point(15, 242);
+            groupBox2.Margin = new Padding(2, 1, 2, 1);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Padding = new Padding(2, 1, 2, 1);
+            groupBox2.Size = new Size(831, 215);
+            groupBox2.TabIndex = 3;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Data";
+            // 
+            // lstVBill
+            // 
+            lstVBill.Location = new Point(4, 18);
+            lstVBill.Margin = new Padding(2, 1, 2, 1);
+            lstVBill.Name = "lstVBill";
+            lstVBill.Size = new Size(810, 182);
+            lstVBill.TabIndex = 0;
+            lstVBill.UseCompatibleStateImageBehavior = false;
+            lstVBill.SelectedIndexChanged += lstVBill_SelectedIndexChanged;
+            // 
+            // WaterBill
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(907, 488);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(btnLogout);
+            Controls.Add(label1);
+            Margin = new Padding(2, 1, 2, 1);
+            Name = "WaterBill";
+            Text = "WaterBill";
+            Load += WaterBill_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btnLogout;
+        private GroupBox groupBox1;
+        private ComboBox cobCustomerType;
+        private Label label3;
+        private TextBox txtFullName;
+        private Label label2;
+        private TextBox txtCurrentMonth;
+        private Label label6;
+        private TextBox txtLastMonthWater;
+        private Label label5;
+        private TextBox txtMember;
+        private Label label4;
+        private Button btnClear;
+        private Button btnEdit;
+        private Button btnAdd;
+        private GroupBox groupBox2;
+        private ListView lstVBill;
+        private Button btnCal;
+        private Label label8;
+        private Label label7;
+        private TextBox txtWatercon;
+        private TextBox txtMoney;
+    }
+}
---
+++ ASM2/ASM2/WaterBill.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ASM2
+{
+    public partial class WaterBill : Form
+    {
+        
+        public WaterBill()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login login = new Login();
+            login.Show();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cobCustomerType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string type = cobCustomerType.Text.Trim().ToLower();
+            if (type.Equals("household customer"))
+            {
+                txtMember.Enabled = true;
+            }
+            else
+            {
+                txtMember.Enabled = false;
+                txtMember.Text = null;
+            }
+
+        }
+
+        private void WaterBill_Load(object sender, EventArgs e)
+        {
+            lstVBill.View = View.Details;
+            lstVBill.GridLines = true;
+            lstVBill.FullRowSelect = true;
+
+            lstVBill.Columns.Add("Full name", 150);
+            lstVBill.Columns.Add("Type", 150);
+            lstVBill.Columns.Add("Member", 100);
+            lstVBill.Columns.Add("Last Water number", 100);
+            lstVBill.Columns.Add("Current Water number", 100);
+            lstVBill.Columns.Add("Water number");
+            lstVBill.Columns.Add("Money" + " VND");
+
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+            
+
+            if (lstVBill.SelectedItems.Count > 0)
+            {
+                btnEdit.Enabled = true;
+                lstVBill.Items.Remove(lstVBill.SelectedItems[0]);
+                ListViewItem item = new ListViewItem();
+                item.Text = txtFullName.Text.Trim();
+                item.SubItems.Add(cobCustomerType.Text.Trim());
+                item.SubItems.Add(txtMember.Text.Trim());
+                item.SubItems.Add(txtLastMonthWater.Text.Trim());
+                item.SubItems.Add(txtCurrentMonth.Text.Trim());
+                item.SubItems.Add(txtWatercon.Text.Trim());
+                item.SubItems.Add(txtMoney.Text.Trim());
+                lstVBill.Items.Add(item);             
+            }
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            string name = txtFullName.Text.Trim();
+            string type = cobCustomerType.Text.Trim();
+            string member = txtMember.Text.Trim();
+            string Nummember = txtMember.Text.Trim();
+            string lastmonthwaterindex = txtLastMonthWater.Text.Trim();
+            string thismonthwaterindex = txtCurrentMonth.Text.Trim();
+
+            
+
+
+
+            
+            ListViewItem item = new ListViewItem(txtFullName.Text.Trim());
+            item.SubItems.Add(cobCustomerType.Text.Trim());
+            item.SubItems.Add(txtMember.Text.Trim());
+            item.SubItems.Add(txtLastMonthWater.Text.Trim());
+            item.SubItems.Add(txtCurrentMonth.Text.Trim());
+            item.SubItems.Add(txtWatercon.Text.Trim());
+            item.SubItems.Add(txtMoney.Text.Trim());
+
+
+            lstVBill.Items.Add(item);
+
+
+        }
+        public double calmoney()
+        {
+            double money = 0;
+            string name = txtFullName.Text.Trim();
+            string type = cobCustomerType.Text;
+            string Nummember = txtMember.Text.Trim();
+            string lastmonthwaterindex = txtLastMonthWater.Text.Trim();
+            string thismonthwaterindex = txtCurrentMonth.Text.Trim();
+            
+            
+            
+
+            if (string.IsNullOrEmpty(name))
+            {
+                MessageBox.Show("Please enter your name", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return money;
+            }
+            if (type=="Household customer")
+            {
+                if (string.IsNullOrEmpty(Nummember))
+                {
+                    MessageBox.Show("Please enter your family member", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return money;
+                }
+
+                else if (int.Parse(Nummember) < 1)
+                {
+                    MessageBox.Show("Number of members must be >= 1", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return money;
+                }
+
+
+                else if (!int.TryParse(lastmonthwaterindex, out _))
+                {
+                    MessageBox.Show("Last month water index must be interger", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return money;
+                }
+
+                else if (!int.TryParse(thismonthwaterindex, out _))
+                {
+                    MessageBox.Show("Current month water index must be interger", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return money;
+                }
+                else if (int.Parse(thismonthwaterindex) - int.Parse(lastmonthwaterindex) < 0 || int.Parse(lastmonthwaterindex) < 0 || int.Parse(thismonthwaterindex) < 0)
+                {
+                    MessageBox.Show("Please Check your water index");
+                    return money;
+                }
+                else
+                {
+                    double usedWater = int.Parse(thismonthwaterindex) - int.Parse(lastmonthwaterindex);
+                    double memberUsedWater = usedWater / int.Parse(Nummember);
+                    if (memberUsedWater <= 10)
+                    {
+                        money = usedWater * 5973 * 1.1;
+                        txtMoney.Text = Convert.ToString(money);
+                        txtWatercon.Text = Convert.ToString(usedWater);
+                        return money;
+                    }
+                    else if (memberUsedWater > 10 && memberUsedWater <= 20)
+                    {
+                        money = usedWater * 7052 * 1.1;
+                        txtMoney.Text = Convert.ToString(money);
+                        txtWatercon.Text = Convert.ToString(usedWater);
+                        return money;
+                    }
+                    else if (memberUsedWater > 20 && memberUsedWater <= 30)
+                    {
+                        money = usedWater * 8699 * 1.1;
+                        txtMoney.Text = Convert.ToString(money);
+                        txtWatercon.Text = Convert.ToString(usedWater);
+                        return money;
+                    }
+                    else
+                    {
+                        money = usedWater * 15929 * 1.1;
+                        txtMoney.Text = Convert.ToString(money);
+                        txtWatercon.Text = Convert.ToString(usedWater);
+                        return money;
+                    }
+
+                }
+            }
+            else 
+            {
+                string lastIndex = txtLastMonthWater.Text.Trim();
+                string thisIndex = txtCurrentMonth.Text.Trim();
+                if (!int.TryParse(lastIndex, out _))
+                {
+                    MessageBox.Show("Please Enter a number!, Error !");
+                    return money;
+                }
+                if (!int.TryParse(thisIndex, out _))
+                {
+                    MessageBox.Show("Please Enter a number!, Error !");
+                    return money;
+                }
+                else if (int.Parse(thisIndex) - int.Parse(lastIndex) < 0 || int.Parse(lastIndex) < 0 || int.Parse(thisIndex) < 0)
+                {
+                    MessageBox.Show("Please Check your water index");
+                    return money;
+                }
+                else
+                {
+                    double usedWater = int.Parse(thisIndex) - int.Parse(lastIndex);
+                    if (cobCustomerType.Text == "Public services")
+                    {
+                        money = usedWater * 9955 * 1.1;
+                        txtMoney.Text = Convert.ToString(money);
+                        txtWatercon.Text = Convert.ToString(usedWater);
+                        return money;
+                    }
+                    
+                    else if (cobCustomerType.Text == "Production units")
+                    {
+                        money = (usedWater * 11615 * 1.1);
+                        txtMoney.Text = Convert.ToString(money);
+                        txtWatercon.Text = Convert.ToString(usedWater);
+                        return money;
+                    }
+                    
+                    else if (cobCustomerType.Text == "Business services")
+                    {
+                        money = usedWater * 22068 * 1.1;
+                        txtMoney.Text = Convert.ToString(money);
+                        txtWatercon.Text = Convert.ToString(usedWater);
+                        return money;
+
+                    }
+                }
+            }
+            return money;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (lstVBill.SelectedItems.Count > 0)
+            {
+                DialogResult dl = MessageBox.Show("Do you want to clear?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dl == DialogResult.Yes)
+                {
+                    lstVBill.Items.Remove(lstVBill.SelectedItems[0]);
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pick one to clear", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Do you want to exit?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialog == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            calmoney();
+            
+            
+        }
+
+        private void txtMoney_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstVBill_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
